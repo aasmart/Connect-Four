a7e91c8992a130c7cbc0f585f0d38cdaeabf017f
aasmart <48771274+aasmart@users.noreply.github.com>
2023-07-03 09:02:40 -0400

    Properly configure environment vars

--- src/main/kotlin/dev/aasmart/plugins/HTTP.kt
+++ src/main/kotlin/dev/aasmart/plugins/HTTP.kt
@@ -5,9 +5,10 @@ import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.application.*
 
 fun Application.configureHTTP() {
+    println("${System.getenv("SITE_URL") ?: "localhost"}:${System.getenv("SITE_PORT") ?: "3000"}")
     install(CORS) {
         allowCredentials = true
-        allowHost(System.getenv("IP") ?: "localhost:3000")
+        allowHost("${System.getenv("SITE_URL") ?: "localhost"}:${System.getenv("SITE_PORT") ?: "3000"}")
         allowMethod(HttpMethod.Post)
         allowHeader(HttpHeaders.ContentType)
     }
