65b6e0f9a0c189b57f096c939911058be17b2326
aasmart <alexandersmart2005@gmail.com>
2023-08-22 14:36:19 -0400

    Fix error caused by unused code

--- src/main/kotlin/dev/aasmart/plugins/Routing.kt
+++ src/main/kotlin/dev/aasmart/plugins/Routing.kt
@@ -1,23 +1,15 @@
 package dev.aasmart.plugins
 
-import dev.aasmart.dao.games.GamesFacade
-import dev.aasmart.game.ConnectFourGame
 import dev.aasmart.routing.games.game
-import dev.aasmart.routing.login.login
 import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.auth.*
-import io.ktor.server.freemarker.*
-import io.ktor.server.http.content.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
         route("/api") {
-            login()
             game()
-
             get {
                 call.respond(HttpStatusCode.OK)
             }
